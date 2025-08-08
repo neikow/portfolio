95ff1b14ad2f00f494867b20dab3c68a3f0b6dc8
vitaly <50383391+Neikow@users.noreply.github.com>
2025-08-08 16:17:19 +0200

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+  ],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-04-03',
+})
