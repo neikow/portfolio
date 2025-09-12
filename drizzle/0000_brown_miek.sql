dccf0e1c2477fb143c57b90228b829f8a7463eca
vitaly <50383391+Neikow@users.noreply.github.com>
2025-09-12 12:15:44 +0200

    migrations
    Took 6 minutes

--- drizzle/0000_brown_miek.sql
+++ drizzle/0000_brown_miek.sql
@@ -8,9 +8,21 @@ CREATE TABLE "blog_posts" (
 	"slug" text NOT NULL,
 	"published" boolean DEFAULT false NOT NULL,
 	"time_published" timestamp with time zone,
-	"last_modified_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"time_edited" timestamp with time zone NOT NULL,
+	"edited_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "blog_posts_slug_unique" UNIQUE("slug")
+);
+--> statement-breakpoint
+CREATE TABLE "experiences" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "experiences_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" text NOT NULL,
+	"role" text NOT NULL,
+	"description" text NOT NULL,
+	"start_date" text NOT NULL,
+	"end_date" text,
+	"company" text NOT NULL,
+	"company_url" text,
+	"logo_url" text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "gallery_images" (
