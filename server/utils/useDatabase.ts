41375205f973cbf5e382cf6c73564b50eab56f01
vitaly <50383391+Neikow@users.noreply.github.com>
2025-09-19 15:45:11 +0200

    logs & temporary disable deploy on push
    Took 1 minute

--- server/utils/useDatabase.ts
+++ server/utils/useDatabase.ts
@@ -2,5 +2,8 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 
 export function useDatabase() {
   const { dbUrl } = useRuntimeConfig()
+
+  console.log({ dbUrl })
+
   return drizzle(dbUrl)
 }
